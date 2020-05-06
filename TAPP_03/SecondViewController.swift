a9ca1344ecf4bd18cd2e79e67c16937ac2c49bdc
MadsJP <mjannerp@gmail.com>
2020-05-06 14:13:24 +0200

    Initial Commit

---
+++ TAPP_03/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TAPP_03
+//
+//  Created by Mads Janner Pedersen on 06/05/2020.
+//  Copyright © 2020 Mads Janner Pedersen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
